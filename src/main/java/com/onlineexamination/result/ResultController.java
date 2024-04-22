a1b6fd84b0c71c62bd58edd1795e116c7705a24a
Pawan0563 <210101120084@cutm.ac.in>
2024-04-23 00:45:19 +0530

    update all module controller

--- src/main/java/com/onlineexamination/result/ResultController.java
+++ src/main/java/com/onlineexamination/result/ResultController.java
@@ -15,38 +15,13 @@ import com.onlineexamination.student.UserRepository;
 
 @RestController
 @CrossOrigin("*")
-//public class ResultController {
-//
-//	  @Autowired
-//	  private ResultRepository resultRepository;
-//	  
-//	  //get all result present in database
-//	  @GetMapping("/result")
-//	  public List<Result> getAllResult(){
-//		  return (List<Result>)this.resultRepository.findAll();
-//	  }
-//	  
-//	  
-//	   //to save result 
-//	  @PostMapping("/result")
-//	  public  Result addNewResult(@RequestBody Result result){
-//		  return this.resultRepository.save(result);
-//	  }
-//	  
-//	   //get all result of a particular student 
-//	  @GetMapping("/user/{email}/result")
-//	  public List<Result> getAllResultForStudent(@PathVariable("email") String email){
-//		  return this.resultRepository.findByEmailEmail(email);
-//		  
-//	  }
-//	  
-//}
-
-
 public class ResultController {
 
     @Autowired
     private ResultService resultService;
+
+    @Autowired
+    private UserRepository userRepository; // Inject UserRepository to save User entities
 
     // Get all results present in the database
     @GetMapping("/result")
@@ -54,9 +29,23 @@ public class ResultController {
         return resultService.getAllResults();
     }
 
-    // Save result 
     @PostMapping("/result")
     public Result addNewResult(@RequestBody Result result) {
+        // Extract user details from the result entity
+        User user = result.getEmail(); // Get the User object directly from the Result entity
+        String userEmail = user.getEmail(); // Retrieve the email from the user object
+        
+        if (userEmail == null || userEmail.isEmpty()) {
+            // If the user email is null or empty, handle it appropriately
+            // For example, you can return an error response or log a message
+        }
+
+        // Now you have the user email, you can use it as needed
+        // For example, you can save it to a database, send an email, etc.
+
+        userRepository.save(user); // Save the user
+
+        result.setEmail(user); // Set the user in the result entity
         return resultService.addNewResult(result);
     }
 
