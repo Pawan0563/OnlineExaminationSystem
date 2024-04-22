2236573661aa56f951c495e3fa130df288667d57
Pawan0563 <210101120084@cutm.ac.in>
2024-04-23 00:48:55 +0530

    update all

--- OnlineExaminationSystemBackendPart/src/main/java/com/onlineexamination/result/Result.java
+++ OnlineExaminationSystemBackendPart/src/main/java/com/onlineexamination/result/Result.java
@@ -6,13 +6,17 @@ import com.onlineexamination.student.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@jakarta.persistence.Entity
+
+
+
+@Entity
 public class Result {
  
 	   @Id
